ffae26a6539b4ed3dfb1db3d0188ad0936ebd429
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-02-25 22:25:03 +0000

    foundation

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,23 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [todolist, setTodolist] = useState([]);
+	const [todos, setTodos] = useState("");
+
+	const updateTodos = e => {
+		setTodos(e.target.values);
+	};
+
+	const Handlead = () => {
+		setTodolist(todolist.concat([todos]));
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div>Todo List</div>
+			<input placeholders="todos" onChange={updateTodos} />
+			<button onClick={Handlead}>add to list</button>
 		</div>
 	);
 }
