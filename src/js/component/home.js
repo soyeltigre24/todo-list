c988f2a7fba457f34a70c0f2dea8fc8f750722a0
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-02-26 21:58:24 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,22 +2,37 @@ import React, { useState } from "react";
 
 //create your first component
 export function Home() {
-	const [todolist, setTodolist] = useState([]);
+	const [todoslist, setTodoslist] = useState([]);
 	const [todos, setTodos] = useState("");
+	let showArray = [];
 
 	const updateTodos = e => {
 		setTodos(e.target.values);
 	};
 
 	const Handlead = () => {
-		setTodolist(todolist.concat([todos]));
+		setTodoslist([...todoslist, todos]);
+		console.log("todos before", todos);
+		setTodos("");
+		console.log("todos after", todos);
+	};
+
+	const showTodoslist = () => {
+		for (let i = 0; todoslist.length; i++)
+			return showArray.push(todoslist[i]);
 	};
 
 	return (
 		<div className="text-center mt-5">
 			<div>Todo List</div>
-			<input placeholders="todos" onChange={updateTodos} />
-			<button onClick={Handlead}>add to list</button>
+			<input onChange={updateTodos} value={todos} />
+			<button className="btn btn-warning" onClick={Handlead}>
+				add to list
+			</button>
+			{todoslist.map((item, index) => (
+				<p key={index}>{item}</p>
+			))}
 		</div>
 	);
 }
+export default Home;
