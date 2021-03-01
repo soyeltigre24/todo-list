cfb39a4107ffb5199b977722216a07989557ed4a
soyeltigre24 <Jairo.Ariza001@mymdc.net>
2021-03-01 22:43:54 +0000

    icon

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 export function Home() {
 	const [todoslist, setTodoslist] = useState([]);
 	const [todos, setTodos] = useState("");
+	const [trash, setTrash] = useState(false);
 	let showArray = [];
 
 	const updateTodos = e => {
@@ -23,14 +24,33 @@ export function Home() {
 	};
 
 	return (
-		<div className="text-center mt-5">
+		<div
+			className="text-center mt-5"
+			style={{ width: "40%", margin: "auto" }}>
 			<div>Todo List</div>
 			<input onChange={e => setTodos(e.target.value)} />
 			<button className="btn btn-warning" onClick={Handlead}>
 				add to list
 			</button>
 			{todoslist.map((item, index) => (
-				<p key={index}>{item}</p>
+				<div
+					key={index}
+					className="border row"
+					unMouseEnter={() => setTrash(!trash)}
+					unMouseLeave={() => setTrash(!trash)}>
+					<div className="d-flex justify-content-start col">
+						<p>{item}</p>
+					</div>
+					<div className="d-flex justify-content-end col">
+						{!trash === false ? (
+							<button>
+								<i className="fas fa-window-close"></i>
+							</button>
+						) : (
+							""
+						)}
+					</div>
+				</div>
 			))}
 		</div>
 	);
